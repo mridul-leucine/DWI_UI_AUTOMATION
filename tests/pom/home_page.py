b5d4faa8f15442fd52bb7358ae4afa47290b68d5
mridul-leucine <mridul.srivastava@leucinetech.com>
2025-11-28 19:46:12 +0530

    cleaning

--- tests/pom/home_page.py
+++
@@ -1,8 +0,0 @@
-class HomePage:
-
-    def __init__(self, page):
-        self.page = page
-        self.cleaning_box = self.page.locator(".use-case-card-body", has_text="Cleaning").click()
-
-    def go_to_home_page(self):
-        self.cleaning_box.click()
